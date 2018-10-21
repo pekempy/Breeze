0e4af30bb9916ab2eb85cadc5399984abb4967db
Peter Kemp <pkemp1992@live.co.uk>
2018-10-21 23:09:30 +0100

    Testing colours

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using MaterialDesignThemes.Wpf;
+using GameLauncher.Views;
 
 namespace GameLauncher
 {
