a1c527eb5c02c0eaecff4d7dc0f9677bbea2c26d
John Campbell <hotrod8254@msn.com>
2018-10-23 14:44:15 -0700

    Edit and Delete V2

--- Views/SettingsView.xaml.cs
+++ Views/SettingsView.xaml.cs
@@ -3,11 +3,13 @@ using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Configuration;
 
 namespace GameLauncher.Views
 {
     public partial class SettingsView : UserControl
     {
+        string theme = "";
         public SettingsView()
         {
             InitializeComponent();
@@ -16,6 +18,7 @@ namespace GameLauncher.Views
         private void DarkModeToggle_Checked(object sender, RoutedEventArgs e)
         {
             ThemeAssist.SetTheme(Application.Current.MainWindow, BaseTheme.Dark);
+            theme = ConfigurationManager.AppSettings["Theme"];
         }
 
         private void DarkModeToggle_Unchecked(object sender, RoutedEventArgs e)
